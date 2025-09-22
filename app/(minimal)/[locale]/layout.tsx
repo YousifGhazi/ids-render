d85c089a24e9d01f267b7f0456d13892d733de04
Yousif Oday <yousefoday22@gmail.com>
2025-09-22 14:56:11 +0300

    fix: removed additional css file

--- app/(minimal)/[locale]/layout.tsx
+++ app/(minimal)/[locale]/layout.tsx
@@ -17,7 +17,6 @@ import { ReactQueryProvider } from "@/providers/react-query-provider";
 
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 
 const CairoFont = Cairo({
   variable: "--font-cairo",
